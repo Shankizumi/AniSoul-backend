278c54522ed0ca53410119131a396df975b86f82
Shashank Gupta <shank005gupta@gmail.com>
2023-04-28 22:00:34 +0530

    project created

---
+++ anisoul/src/main/java/com/ts/anisoul/AnisoulApplication.java
@@ -0,0 +1,13 @@
+package com.ts.anisoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnisoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnisoulApplication.class, args);
+	}
+
+}
---
+++ anisoul/src/test/java/com/ts/anisoul/AnisoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.anisoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnisoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
