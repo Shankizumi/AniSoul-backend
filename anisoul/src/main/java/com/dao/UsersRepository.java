b85495aaac791961addb215ad8496144e4bb6fdf
Shashank Gupta <shank005gupta@gmail.com>
2023-05-09 12:55:17 +0000

    Update UsersRepository.java

--- anisoul/src/main/java/com/dao/UsersRepository.java
+++ anisoul/src/main/java/com/dao/UsersRepository.java
@@ -10,6 +10,8 @@ public interface UsersRepository extends JpaRepository<Users,Integer> {
 	@Query(" select e.password from Users e where e.userName = :userName ")
 	public String userAuthentication(@Param("userName") String userName);
 	
+		@Query(" from Users e where e.userName = :userName ")
+	public Users userByUserName(@Param("userName")  String userName);
 	
 	
 }
